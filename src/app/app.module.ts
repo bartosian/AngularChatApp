f2995aae82b6976971c934cfe231a7a1a1c62c9c
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-15 10:56:21 -0400

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import {BsDropdownModule} from 'ngx-bootstrap';
 import { LoginComponent } from './pages/login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {SignupComponent} from './pages/signup/signup.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ChatComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

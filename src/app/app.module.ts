cb12586971fab5299e447d46a46caca677bfed68
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-15 11:15:41 -0400

    Finished starting pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Modules
+import { AlertModule } from 'ngx-bootstrap';
+import { LoadingModule } from 'ngx-loading';
+
+// Guards
+import { AuthGuard } from './guards/auth.guard';
+
+// Services
+import { AlertService } from './services/alert.service';
+import { AuthService } from './services/auth.service';
+import { LoadingService } from './servies/loading.service';
+
+// Components
 import { AppComponent } from './app.component';
-import {BsDropdownModule} from 'ngx-bootstrap';
 import { LoginComponent } from './pages/login/login.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {SignupComponent} from './pages/signup/signup.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { ChatComponent } from './pages/chat/chat.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatInputComponent } from './pages/chat/components/chat-input/chat-input.component';
+import { ChatroomListComponent } from './pages/chat/components/chatroom-list/chatroom-list.component';
+import { ChatroomTitleBarComponent } from './pages/chat/components/chatroom-title-bar/chatroom-title-bar.component';
+import { ChatMessageComponent } from './pages/chat/components/chat-message/chat-message.component';
+import { ChatroomWindowComponent } from './pages/chat/components/chatroom-window/chatroom-window.component';
+
 
 @NgModule({
   declarations: [
@@ -16,15 +34,27 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     LoginComponent,
     SignupComponent,
     ChatComponent,
-    NavbarComponent
+    NavbarComponent,
+    ChatInputComponent,
+    ChatroomListComponent,
+    ChatroomTitleBarComponent,
+    ChatMessageComponent,
+    ChatroomWindowComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BsDropdownModule.forRoot(),
-    ReactiveFormsModule
+    AlertModule.forRoot(),
+    ReactiveFormsModule,
+    FormsModule,
+    LoadingModule
   ],
-  providers: [],
+  providers: [
+    AlertService,
+    LoadingService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertType } from './../../enums/alert-type.enum';
+import { Alert } from './../../classes/alert';
+import { AlertService } from './../../services/alert.service';
+import { Subscription } from 'rxjs/Subscription';
+import { LoadingService } from './../../servies/loading.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   public loginForm: FormGroup;
+  private subscriptions: Subscription[] = [];
+  private returnUrl: string;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder, 
+    private alertService: AlertService,
+    private loadingService: LoadingService,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.createForm();
-  }
+   }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/chat';
   }
 
   private createForm(): void {
@@ -24,10 +41,30 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  public submitForm(): void {
-    const { email, password } = this.loginForm.value;
+  public submit(): void {
+    
+    if (this.loginForm.valid) {
+      this.loadingService.isLoading.next(true);
+      const {email, password} = this.loginForm.value;
 
-    console.log(email, password);
+      // TODO call the auth service
+      this.subscriptions.push(
+        this.auth.login(email, password).subscribe(success => {
+          if (success) {
+            this.router.navigateByUrl(this.returnUrl);
+          }
+          this.loadingService.isLoading.next(false);
+        })
+      );
+    } else {
+      const failedLoginAlert = new Alert('Your email or password were inavalid, try again.', AlertType.Danger);
+      this.loadingService.isLoading.next(false);
+      this.alertService.alerts.next(failedLoginAlert);
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
